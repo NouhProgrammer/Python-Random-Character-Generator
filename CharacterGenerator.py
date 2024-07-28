df660d6d70c477ebd2be14cb658e755df9fe7e89
NouhProgrammer <chahinnouh5@gmail.com>
2024-07-28 17:27:16 +0300

    Bug fixes

--- CharacterGenerator.py
+++ CharacterGenerator.py
@@ -1,5 +1,5 @@
 class Generator:
-    def generate(self, nr_chars, nr_numbers, nr_symbols)
+    def generate(self, nr_chars, nr_numbers, nr_symbols):
         letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
